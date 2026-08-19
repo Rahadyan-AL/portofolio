4d5f2d093ee06b0f132cf350d66805396183a9ad
Rahadyan Al F <2008rahadyanaal@gmail.com>
2026-08-19 11:05:07 +0700

    i dont know what to say

--- my-web/app/landing/page.tsx
+++ my-web/app/landing/page.tsx
@@ -1,8 +1,19 @@
 import { RetroGrid } from "@/components/ui/retro-grid"
+import { TextAnimate } from "@/components/ui/text-animate"
+import {ComicText} from "@/components/ui/comic-text"
 export default function LandingPage() {
     return (
-        <div className="relative h-[500px] w-full overflow-hidden">
-            <RetroGrid />
+        <div className="relative w-full overflow-hidden">
+            
+            <TextAnimate 
+                className="items-center justify-center"
+                animation="fadeIn" 
+                by="line" 
+                as="p">
+                {`Fade in by line as paragraph\n\nFade in by line as paragraph\n\nFade in by line as paragraph`}
+            </TextAnimate>
+
+            <ComicText>Silahkan pilih mau tema apa</ComicText>
         </div>
     )
 }
